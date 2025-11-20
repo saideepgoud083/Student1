88ef8fd82c53934df2f64e95372ea73501fa4481
DCstart <dhanush.21bce8699@vitapstudent.ac.in>
2025-11-20 15:05:13 +0530

    f

--- src/studentpac1/studenteimplementation.java
+++ src/studentpac1/studenteimplementation.java
@@ -7,13 +7,24 @@ public class studenteimplementation implements operations1  {
 	@Override
 	public studente findStudent(int id, ArrayList<studente> studlist) {
 		// TODO Auto-generated method stub
-		return null;
+		studente s=null;
+		for(int i=0;i<studlist.size();i++)
+		{
+			if(studlist.get(i).getSid()==id) 
+			{
+				s = studlist.get(i);
+				break;
+			}
+		}
+		return s;
+
 	}
 
 	@Override
 	public void addStudent(studente s, ArrayList<studente> studlist) {
 		// TODO Auto-generated method stub
-		
+		studlist.add(s);
+
 	}
 
 	@Override
